899b83b4eead88f6a2d750a19aeba1fb3e6effed
peterGork <159734183+peterGork@users.noreply.github.com>
2024-02-18 14:02:00 +0300

    added theory

---
+++ myHomeWork2.py
@@ -0,0 +1,31 @@
+# # print("hello \n \t this is my first homework")
+# # # print("\n" * 4)
+# # # print("fill", end="-" "TO:")
+# # # num1 = 800
+# # # num2 = 80
+# # # print(num1 // num2)
+# # # print(num1 % num2)
+# #
+# # number = int(input("Enter 4-digit number:"))
+# # n1 = number // 1000
+# # n2 = number // 100 % 10
+# # n3 = number % 100 // 10
+# # n4 = number % 10
+# #
+# # result = n1 + n2 + n3 + n4
+# # print(f"n1: {n1} n2: {n2} n3: {n3} n4: {n4}")
+# # # print(f"Result: {result}")
+#
+# first_diagonal=int(input("Enter the first diagonal:"))
+# second_diagonal = int(input("enter the second diagonal:"))
+# area = (first_diagonal * second_diagonal) / 2
+# print("Area:", area)
+#
+# print("hello", "hello","hello", sep="v,", end=" ")
+# print("world")
+#
+#
+# print("hello", "hello","hello", sep="v,", end=" ")
+# print("world")
+#
+# #
